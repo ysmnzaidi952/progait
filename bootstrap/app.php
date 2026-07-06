5548e7c655a04d63c697a70d7fe7689a98c5986d
Yasmin <ysmnzaidi@gmail.com>
2026-07-06 11:51:16 +0800

    Trust all proxies to fix HTTPS asset URLs on Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->redirectUsersTo(function (Request $request) {
             if(Auth::guard("patient")->check()){
                 return route('patient.indexpatient');
